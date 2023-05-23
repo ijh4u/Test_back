1c2ae123fbdd876ad632191192ad8c75093e5eb5
ijh4u <ijh4u1@naver.com>
2023-05-23 20:10:14 +0900

    first

---
+++ src/main/java/com/mysite/highfive/HighfiveApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.highfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighfiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighfiveApplication.class, args);
+	}
+
+}
